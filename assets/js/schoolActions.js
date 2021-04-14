1f7437ce04d5955862736362d0eb17a4317787b8
jackn11 <jacknaimer@gmail.com>
2021-04-14 18:29:11 -0400

    Update schoolActions.js

---
+++ assets/js/schoolActions.js
@@ -0,0 +1,46 @@
+import {db} from "./firebase.js"
+
+function addExperience(description, location, name, visibility)
+{
+	db.collection("schools")
+        .doc(firebase.auth().currentUser.uid)
+        .collection("opportunities")
+        .add({
+          description: description,
+          location: location,
+          name: name,
+          visibility
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+}
+
+
+function editExperience(description, location, name, visibility, id)
+{
+	db.collection("schools")
+        .doc(firebase.auth().currentUser.uid)
+        .collection("opportunities")
+        .doc(id)
+        .set({
+          experience: experience,
+          position: position
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+}
+
+
+function deleteExperience(id)
+{
+	db.collection("schools")
+        .doc(firebase.auth().currentUser.uid)
+        .collection("opportunities")
+        .doc(id).delete().then(() => {
+    		console.log("Document successfully deleted!");
+		}).catch((error) => {
+    	console.error("Error removing document: ", error);
+	});
+}
