54ede31a515e59fa7dea12f979911ccbef4d8442
Jwiggiff <friedman.josh03@gmail.com>
2021-04-14 18:06:54 -0400

    add uploading files

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -22,167 +22,200 @@ function initializeInstance() {
   storageRef = storage.ref();
 }
 
-export function registerVolunteer(age, email, password, firstName, lastName, interests, location, timeCommitment, timeFrame) {
-	firebase
+export function registerVolunteer(
+  age,
+  email,
+  password,
+  firstName,
+  lastName,
+  interests,
+  postalCode,
+  timeCommitment,
+  timeFrame
+) {
+  firebase
     .auth()
     .createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
       let user = userCredential.user;
 
-      user.sendEmailVerification().then(function() {
-		// Email sent.
-		}).catch(function(error) {
-		// An error happened.
-	  });
+      user.sendEmailVerification().catch(function (error) {
+        console.error(error.code, error.message);
+      });
 
-
-		user.updateProfile({
-		  displayName: firstName+" "+lastName,
-		}).then(function() {
-		  // Update successful.
-		}).catch(function(error) {
-		  // An error happened.
-		});
+      user
+        .updateProfile({
+          displayName: firstName + " " + lastName,
+        })
+        .catch(function (error) {
+          console.error(error.code, error.message);
+        });
 
       db.collection("volunteers")
         .doc(user.uid)
         .set({
           age: age,
           email: email,
-		  firstName: firstName,
-		  lastName: lastName,
+          firstName: firstName,
+          lastName: lastName,
           interests: interests,
-          location: location,
+          postalCode: postalCode,
           timeCommitment: timeCommitment,
-          timeFrame: timeFrame
+          timeFrame: timeFrame,
         })
         .catch((error) => {
           console.error("Error adding document: ", error);
         });
     })
     .catch((error) => {
-      console.error(err.code, err.message);
+      console.error(error.code, error.message);
     });
 }
 
-export function registerOrganization(email, password, website, location, fields, description, logo, bg_img, phoneNumber) {
-	firebase
-	.auth()
-	.createUserWithEmailAndPassword(email, password)
-	.then((userCredential) => {
-		let user = userCredential.user;
+export function registerOrganization(
+  name,
+  email,
+  password,
+  website,
+  location,
+  fields,
+  description,
+  logo,
+  bg_img,
+  phoneNumber
+) {
+  var fileName = "logo." + logo.type.split("/")[1];
+  console.log(name + "/" + fileName);
 
-		user.sendEmailVerification().then(function() {
-			// Email sent.
-			}).catch(function(error) {
-			// An error happened.
-	  	});
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      let user = userCredential.user;
 
-		user.updateProfile({
-		  displayName: name,
-		}).then(function() {
-		  // Update successful.
-		}).catch(function(error) {
-		  // An error happened.
-		});
+      user.sendEmailVerification().catch(function (error) {
+        console.error(error.code, error.message);
+      });
 
-		db.collection("organizations")
-			.doc(user.uid)
-			.set({
-				name: name,
-				email: email,
-				website: website,
-				location: location,
-				phone: phoneNumber
-			})
-			.then((docRef) => {
-				console.log("Document written with ID: ", docRef.id);
-			})
-			.catch((error) => {
-				console.error("Error adding document: ", error);
-			});
-		})
-		.catch((error) => {
-			console.error(err.code, err.message);
-		});
+      user
+        .updateProfile({
+          displayName: name,
+        })
+        .catch(function (error) {
+          console.error(error.code, error.message);
+        });
 
-		var imagesRef = storageRef.child(name);
+      let logoName = "logo." + logo.type.split("/")[1];
+      let logoRef = storageRef.child(name + "/" + logoName);
+      logoRef.put(logo).catch((error) => {
+        console.error(error.code, error.message);
+      });
 
-		var fileName = "logo"+logo.type.split("/")[1]
+      if (bg_img) {
+        let bgName = "background." + bg_img.type.split("/")[1];
+        let bgRef = storageRef.child(name + "/" + bgName);
+        bgRef.put(bg_img).catch((error) => {
+          console.error(error.code, error.message);
+        });
+      }
 
-		var spaceRef = imagesRef.child(fileName)
-
-		var path = spaceRef.fullPath;
-
-		var name = spaceRef.name;
-
-		var imagesRef = spaceRef.parent;
+      db.collection("organizations")
+        .doc(user.uid)
+        .set({
+          name: name,
+          email: email,
+          website: website,
+          location: location,
+          phone: phoneNumber,
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+    })
+    .catch((error) => {
+      console.error(error.code, error.message);
+    });
 }
 
-export function registerSchool(name, email, password, website, location, phoneNumber) {
-	firebase
-	.auth()
-	.createUserWithEmailAndPassword(email, password)
-	.then((userCredential) => {
-		let user = userCredential.user;
+export function registerSchool(
+  name,
+  email,
+  password,
+  website,
+  location,
+  phoneNumber,
+  logo,
+  bg_img
+) {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      let user = userCredential.user;
 
-		user.sendEmailVerification().then(function() {
-			// Email sent.
-			}).catch(function(error) {
-			// An error happened.
-	  	});
+      user.sendEmailVerification().catch(function (error) {
+        console.error(error.code, error.message);
+      });
 
-		user.updateProfile({
-		  displayName: name,
-		}).then(function() {
-		  // Update successful.
-		}).catch(function(error) {
-		  // An error happened.
-		});
+      user
+        .updateProfile({
+          displayName: name,
+        })
+        .catch(function (error) {
+          console.error(error.code, error.message);
+        });
 
-		db.collection("school")
-			.doc(user.uid)
-			.set({
-				name: name,
-				email: email,
-				website: website,
-				location: location,
-				phone: phoneNumber
-			})
-			.then((docRef) => {
-				console.log("Document written with ID: ", docRef.id);
-			})
-			.catch((error) => {
-				console.error("Error adding document: ", error);
-			});
-		})
-		.catch((error) => {
-			console.error(err.code, err.message);
-		});
+      let logoName = "logo." + logo.type.split("/")[1];
+      let logoRef = storageRef.child(name + "/" + logoName);
+      logoRef.put(logo).catch((error) => {
+        console.error(error.code, error.message);
+      });
+
+      if (bg_img) {
+        let bgName = "background." + bg_img.type.split("/")[1];
+        let bgRef = storageRef.child(name + "/" + bgName);
+        bgRef.put(bg_img).catch((error) => {
+          console.error(error.code, error.message);
+        });
+      }
+
+      db.collection("school")
+        .doc(user.uid)
+        .set({
+          name: name,
+          email: email,
+          website: website,
+          location: location,
+          phone: phoneNumber,
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+    })
+    .catch((error) => {
+      console.error(error.code, error.message);
+    });
 }
 
-
-export function login(email, password)
-{
-	firebase.auth().signInWithEmailAndPassword(email, password)
-	.then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
+export function login(email, password) {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch((error) => {
+      console.error(error.code, error.message);
+    });
 }
 
-export function logout()
-{
-	firebase.auth().signOut().then(() => {
-  		// Sign-out successful.
-	}).catch((error) => {
-  		// An error happened.
-	});
+export function logout() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
 }
 
 initializeInstance();
