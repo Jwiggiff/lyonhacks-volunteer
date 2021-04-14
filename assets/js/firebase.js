63e5441ff5d39f43d71c6ee858aed8a9a9748642
jackn11 <jacknaimer@gmail.com>
2021-04-14 12:01:30 -0400

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -103,5 +103,17 @@ function registerSchool(email, password, website, location) {
 
 
 function login(email, password)
+{
+	firebase.auth().signInWithEmailAndPassword(email, password)
+	.then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+}
 
 initializeInstance();
