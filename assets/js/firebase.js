53792cda8ed954e50119d8071d170df5a89d692a
Jwiggiff <friedman.josh03@gmail.com>
2021-04-14 11:05:34 -0400

    update registerVolunteer method

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,43 +1,52 @@
-function initializeInstance () {
-	// Initialize Cloud Firestore through Firebase
-	//I think that this function is supposed to be called before the user logs in
-	firebase.initializeApp({
-	  apiKey: 'AIzaSyDqyIRkw4LBVXwHPSvryG0-YKBGXrK0TgE',
-	  authDomain: 'lyonshack-e36ac.firebaseapp.com',
-	  projectId: 'lyonshack-e36ac'
-	});
+let db;
 
-	var db = firebase.firestore();
+function initializeInstance() {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDqyIRkw4LBVXwHPSvryG0-YKBGXrK0TgE",
+    authDomain: "lyonshack-e36ac.firebaseapp.com",
+    projectId: "lyonshack-e36ac",
+    storageBucket: "lyonshack-e36ac.appspot.com",
+    messagingSenderId: "286855859814",
+    appId: "1:286855859814:web:4d7aa3770753d5999c67a7",
+    measurementId: "G-RHY84JFRRG",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  db = firebase.firestore();
 }
 
-function register(age, email, password, interests, location, timeCommitment, timeSpent) {
+function registerVolunteer(age, email, firstName, lastName, interests, location, timeCommitment, timeFrame) {
+	firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      let user = userCredential.user;
 
-	firebase.auth().createUserWithEmailAndPassword(email, password)
-	  .then((userCredential) => {
-	    // Signed in 
-	    var user = userCredential.user;
-	    // ...
-	  })
-	  .catch((error) => {
-	    var errorCode = error.code;
-	    var errorMessage = error.message;
-	    // ..
-	  });
-
-
-	db.collection("Volunteer").add({
-    	age: age,
-    	email: email,
-    	password: password,
-    	interests: interests,
-    	location: location,
-    	timeCommitment: timeCommitment,
-    	timeSpent: timeSpent
-    })
-    .then((docRef) => {
-    	console.log("Document written with ID: ", docRef.id);
+      db.collection("volunteers")
+        .doc(user.uid)
+        .set({
+          age: age,
+          email: email,
+					firstName: firstName,
+					lastName: lastName,
+          interests: interests,
+          location: location,
+          timeCommitment: timeCommitment,
+          timeFrame: timeFrame,
+        })
+        .then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
     })
     .catch((error) => {
-    	console.error("Error adding document: ", error);
-	});
+      console.error(err.code, err.message);
+    });
 }
+
+initializeInstance();
