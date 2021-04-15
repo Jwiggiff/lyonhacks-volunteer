ea3a189f582850ea6eca52e9665f47dc68c31491
jackn11 <jacknaimer@gmail.com>
2021-04-14 20:43:50 -0400

    Update comment.js

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -6,11 +6,42 @@ export function addComment(comment, userID, opportunityID)
         .doc(firebase.auth().currentUser.uid)
         .collection("opportunities")
         .doc(opportunityID)
-        .set({
+        .collection("comments")
+        .add({
           userID: userID,
           comment: comment
         })
         .catch((error) => {
           console.error("Error adding document: ", error);
         });
+}
+
+//The frontend should ensure that this function can only run if the user that is logged in is the one that made the comment
+export function editComment(comment, opportunityID, commentID)
+{
+  db.collection("organizations")
+        .doc(firebase.auth().currentUser.uid)
+        .collection("opportunities")
+        .doc(opportunityID)
+        .collection("comments")
+        .doc(commentID)
+        .set({
+          comment: comment
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+}
+
+//The frontend should ensure that this function can only run if the user that is logged in is the one that made the comment
+export function deleteExperience(commentID)
+{
+  db.collection("organizations")
+        .doc(firebase.auth().currentUser.uid)
+        .collection("opportunities")
+        .doc(commentID).delete().then(() => {
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+      console.error("Error removing document: ", error);
+  });
 }
