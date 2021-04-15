6d8ac256127580e6b16774815ea97470be4f0444
jackn11 <jacknaimer@gmail.com>
2021-04-14 20:08:43 -0400

    Update organizationActions.js

---
+++ assets/js/organizationActions.js
@@ -0,0 +1,56 @@
+import {db} from "./firebase.js"
+
+function addExperience(position, location, timeFrame, timeCommitment, requirements, description, contact, status) //status is for whether the opportunity is ongoing or archived
+{
+	db.collection("organizations")
+        .doc(firebase.auth().currentUser.uid)
+        .collection("opportunities")
+        .add({
+          position: position,
+          location: location,
+          timeFrame: timeFrame,
+          timeCommitment: timeCommitment,
+          requirements: requirements,
+          description: description,
+          contact: contact,
+          status: status
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+}
+
+
+function editExperience(position, location, timeFrame, timeCommitment, requirements, description, contact, status, id)
+{
+	db.collection("organizations")
+        .doc(firebase.auth().currentUser.uid)
+        .collection("opportunities")
+        .doc(id)
+        .set({
+          position: position,
+          location: location,
+          timeFrame: timeFrame,
+          timeCommitment: timeCommitment,
+          requirements: requirements,
+          description: description,
+          contact: contact,
+          status: status
+        })
+        .catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+}
+
+
+function deleteExperience(id)
+{
+	db.collection("organizations")
+        .doc(firebase.auth().currentUser.uid)
+        .collection("opportunities")
+        .doc(id).delete().then(() => {
+    		console.log("Document successfully deleted!");
+		}).catch((error) => {
+    	console.error("Error removing document: ", error);
+	});
+}
